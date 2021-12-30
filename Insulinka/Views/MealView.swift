4a41fd9b5d3f0dc49656158cac82bddae7da40c4
Wiktor <wiktor.tuminski@gmail.com>
2021-12-30 19:39:16 +0100

    Updated MealView

--- Insulinka/Views/MealView.swift
+++ Insulinka/Views/MealView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct MealView: View {
+    let meal: Meal
+    @State static var isEditing: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List{
+                Section {
+                    SectionItem(mainText: "Nazwa posiłku", secondaryText: meal.name!)
+                }
+                Section {
+                    SectionItem(mainText: "Tłuszcze", secondaryText: String(meal.fats))
+                    SectionItem(mainText: "Węglowodany", secondaryText: String(meal.carbs))
+                    SectionItem(mainText: "Białka", secondaryText: String(meal.proteins))
+                    SectionItem(mainText: "Dawka insuliny na 100g posiłku", secondaryText: String(meal.insulinDosePer100G))
+                    
+                }
+//                InsulinCalculatorView()
+            }
+            .listStyle(.insetGrouped)
+            .navigationBarHidden(true)
+            .toolbar {
+                
+            }
+        }.navigationTitle(Text("Posiłek"))
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct MealView_Previews: PreviewProvider {
     static var previews: some View {
-        MealView()
+        let viewContext = DataController().persistentContainer.viewContext
+        let meal = Meal.init(context: viewContext)
+        meal.id = UUID()
+        meal.name = "test meal"
+        meal.insulinDosePer100G = 0.00
+        return MealView(meal: meal)
+            .environment(\.managedObjectContext, viewContext)
     }
 }
